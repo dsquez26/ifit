05d98e076c94a3aa100612e9c405fe7784a1afe5
Your Name <davidvazquez1026@gmail.com>
2020-05-29 18:54:36 -0500

    Initial commit for ifit challenge

---
+++ src/Components/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function DemoFooter() {
+    return (
+  
+        <div className="legal" id="test">
+          <div className="legal__links">
+            <span className="legal__link">© iFit.com. All Rights Reserved.</span>
+            <span className="legal__link">Privacy Policy</span>
+            <span className="legal__link">Terms of Use</span>
+          </div>
+        </div>
+   );
+}
+
+export default DemoFooter;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import DemoFooter from "components/Footers/DemoFooter.js";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
+  document.getElementById('test')
 );
 
 // If you want your app to work offline and load faster, you can change
